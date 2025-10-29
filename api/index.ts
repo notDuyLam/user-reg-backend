f1cf7f9aa0c7ee498ef989f1105ea8d823fd49b4
notDuyLam <22120181@student.hcmus.edu.vn>
2025-10-29 20:40:38 +0700

    fix: config for deploy

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import express from 'express';
 import serverless from 'serverless-http';
-import { AppModule } from '../src/app.module';
+import { AppModule } from '../src/app.module.js';
 
 let server: any;
 
